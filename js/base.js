dcd78bbfe87ecf5c796eea535ad4c1108c1d84e1
aaiit <abdellatif.aithammadi@gmail.com>
2021-02-27 16:20:50 +0100

    ++

---
+++ js/base.js
@@ -0,0 +1,106 @@
+header =`<div class="zelda-responsive-nav">
+<div class="container">
+<div class="zelda-responsive-menu">
+<div class="logo">
+<a href="index.html">
+<img src="images/img-logo.png" alt="logo">
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="zelda-nav">
+<div class="container-fluid">
+<nav class="navbar navbar-expand-md navbar-light">
+<a class="navbar-brand" href="index.html">
+<img src="images/img-logo.png" alt="logo">
+</a>
+<div class="collapse navbar-collapse mean-menu">
+<ul class="navbar-nav">
+<li class="nav-item"><a href="#" class="nav-link active">Home </a>
+
+</li>
+<li class="nav-item"><a href="#" class="nav-link">Platform <i class="flaticon-down-arrow"></i></a>
+<ul class="dropdown-menu">
+ <li class="nav-item"><a href="Platform.html?a=Upcoming" class="nav-link">Upcoming</a></li>
+ <li class="nav-item"><a href="Platform.html?a=Popular" class="nav-link">Popular</a></li>
+ <li class="nav-item"><a href="Platform.html?a=RPG" class="nav-link">RPG</a></li>
+ <li class="nav-item"><a href="Platform.html?a=Sport" class="nav-link">Sport</a></li>
+</ul>
+</li>
+<li class="nav-item"><a href="#" class="nav-link">Tournament <i class="flaticon-down-arrow"></i></a>
+<ul class="dropdown-menu">
+<li class="nav-item"><a href="stream-schedule.html" class="nav-link">Stream Schedule</a></li>
+<li class="nav-item"><a href="single-tournament.html" class="nav-link">Tournament Single</a></li>
+<li class="nav-item"><a href="single-match.html" class="nav-link">Match Single</a></li>
+<li class="nav-item"><a href="single-team.html" class="nav-link">Team Single</a></li>
+<li class="nav-item"><a href="single-player.html" class="nav-link">Player Single</a></li>
+</ul>
+</li>
+<!--<li class="nav-item"><a href="#" class="nav-link">Shop <i class="flaticon-down-arrow"></i></a>-->
+<!--<ul class="dropdown-menu">-->
+<!--<li class="nav-item"><a href="products-list-1.html" class="nav-link">Products List 01</a></li>-->
+<!--<li class="nav-item"><a href="products-list-2.html" class="nav-link">Products List 02</a></li>-->
+<!--<li class="nav-item"><a href="cart.html" class="nav-link">Cart</a></li>-->
+<!--<li class="nav-item"><a href="checkout.html" class="nav-link">Checkout</a></li>-->
+<!--<li class="nav-item"><a href="single-products.html" class="nav-link">Products Details</a></li>-->
+<!--<li class="nav-item"><a href="my-account.html?html" class="nav-link">My Account</a></li>-->
+<!--</ul>-->
+<!--</li>-->
+<li class="nav-item"><a href="#" class="nav-link">Community <i class="flaticon-down-arrow"></i></a>
+<ul class="dropdown-menu">
+
+<li class="nav-item"><a href="single-blog-1.html" class="nav-link">Blog</a></li>
+<li class="nav-item"><a href="single-blog-2.html" class="nav-link">Forum</a></li>
+ <li class="nav-item"><a href="single-blog-3.html" class="nav-link">Twitch</a></li>
+ <li class="nav-item"><a href="single-blog-3.html" class="nav-link">Vimeo</a></li>
+ <li class="nav-item"><a href="single-blog-3.html" class="nav-link">Youtube</a></li>
+</ul>
+</li>
+<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+</ul>
+<div class="others-option d-flex align-items-center">
+<div class="option-item">
+<div class="search-box">
+<i class="flaticon-search-1"></i>
+</div>
+</div>
+<div class="option-item">
+<div class="side-menu-btn">
+<i class="flaticon-null-2" data-toggle="modal" data-target="#sidebarModal"></i>
+</div>
+</div>
+</div>
+</div>
+</nav>
+</div>
+</div>
+<div class="others-option-for-responsive">
+<div class="container">
+<div class="dot-menu">
+<div class="inner">
+<div class="circle circle-one"></div>
+<div class="circle circle-two"></div>
+<div class="circle circle-three"></div>
+</div>
+</div>
+<div class="container">
+<div class="option-inner">
+<div class="others-option">
+<div class="option-item">
+<div class="search-box">
+<i class="flaticon-search-1"></i>
+</div>
+</div>
+<div class="option-item">
+<div class="side-menu-btn">
+<i class="flaticon-null-2" data-toggle="modal" data-target="#sidebarModal"></i>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>`;
+
+$(".navbar-area").html(header);
